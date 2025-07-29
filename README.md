4d9582e671f7b04140f3f50f6a061a3a795c1d92
sittipong03 <sittipong.f@gmail.com>
2025-07-29 09:43:55 +0700

    start project

---
+++ README.md
@@ -0,0 +1,8 @@
+package installed
+- tailwind
+- react-router
+- axios
+- react-hook-form
+- @hookform/resolvers
+- yup
+- zustand
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter ,RouterProvider } from "react-router"
+import { lazy, Suspense } from "react"
+
+
+function AppRouter() {
+  return (
+    <div>AppRouter</div>
+  )
+}
+export default AppRouter

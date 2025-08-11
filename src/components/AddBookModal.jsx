88670c9a585859be5df6cf58030d5a9c9f674870
natchaphatso <natchaphatso@gmail.com>
2025-08-11 20:00:07 +0700

    feat: edit more layout in addbookmodal

--- src/components/AddBookModal.jsx
+++ src/components/AddBookModal.jsx
@@ -159,7 +159,7 @@ const BookSearchModal = ({ isOpen, onClose, onBookSelect }) => {
           <div className="flex w-full items-center justify-between gap-3">
             <div className="relative flex w-full items-center justify-between gap-4">
               <div className="absolute top-1/2 left-5 -translate-y-1/2 transform">
-                <i className="fa-solid fa-book-open-cover text-text-action-disabled"></i>
+                <i className="fa-solid fa-book-open-cover text-text-disabled"></i>
               </div>
               <Input
                 type="text"
